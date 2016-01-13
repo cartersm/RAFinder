1881ff4240f39342c39f5794e9b9de0069cb1b19
cartersm <cartersm@rose-hulman.edu>
2016-01-13 18:18:13 -0500

    Add modal service

--- Web/RAFinder/app/app.js
+++ Web/RAFinder/app/app.js
@@ -82,7 +82,8 @@ angular.module('RAFinder', [
                 }
             };
 
-            this.checkAuth(authObj.$getAuth(), function () {
+            var auth = authObj.$getAuth();
+            this.checkAuth(auth, function () {
                 user = auth.password.email;
             });
 
@@ -107,4 +108,55 @@ angular.module('RAFinder', [
                 },
                 checkAuth: this.checkAuth
             }
-        }]);
+        }
+    ])
+    // the following is borrowed from http://weblogs.asp.net/dwahlin/building-an-angularjs-modal-service
+    .service("ModalService", ['$uibModal',
+        function ($uibModal) {
+            var modalDefaults = {
+                backdrop: true,
+                keyboard: true,
+                modalFade: true,
+                templateUrl: '/app/partials/modal.html'
+            };
+
+            var modalOptions = {
+                closeButtonText: 'Close',
+                actionButtonText: 'OK',
+                headerText: 'Proceed?',
+                bodyText: 'Perform this action?'
+            };
+
+            this.showModal = function (customModalDefaults, customModalOptions) {
+                if (!customModalDefaults) customModalDefaults = {};
+                customModalDefaults.backdrop = 'static';
+                return this.show(customModalDefaults, customModalOptions);
+            };
+
+            this.show = function (customModalDefaults, customModalOptions) {
+                //Create temp objects to work with since we're in a singleton service
+                var tempModalDefaults = {};
+                var tempModalOptions = {};
+
+                //Map angular-ui modal custom defaults to modal defaults defined in service
+                angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
+
+                //Map modal.html $scope custom properties to defaults defined in service
+                angular.extend(tempModalOptions, modalOptions, customModalOptions);
+
+                if (!tempModalDefaults.controller) {
+                    tempModalDefaults.controller = function ($scope, $modalInstance) {
+                        $scope.modalOptions = tempModalOptions;
+                        $scope.modalOptions.ok = function (result) {
+                            $modalInstance.close(result);
+                        };
+                        $scope.modalOptions.close = function (result) {
+                            $modalInstance.dismiss('cancel');
+                        };
+                    }
+                }
+
+                return $uibModal.open(tempModalDefaults).result;
+            };
+        }
+    ]);
